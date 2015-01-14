c4873a408297c54b4085c30bcdfb7bda31dd9508
estebangallego <egallego00@gmail.com>
2015-01-13 20:08:37 -0800

    ninjaBeta

--- jquery.js
+++ jquery.js
@@ -1,20 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function(){	
+		
+	$( ".ninjaImgShow" ).click(function() {
+		$( this ).fadeOut( "slow" );
+	});
 
-		$('form').submit(function(){
-		$('.users').append(
-			'<tr><td>' + $("#firstName").val() +'</td>' + 
-			'<td>' + $("#lastName ").val() +'</td>' +
-			'<td>' + $("#emailAddress ").val() +'</td>' +
-			'<td>' + $("#number ").val() +'</td></tr>'
-			);
-		return false;
-		})
-
-
-
-	// $('form').submit(function(){
-	// 	// alert('test')
-	// 	$( '#test' ).append( $( 'text' ) );
-	// })
-})
-// <td>First Name</td><td>Last Name</td><td>Email Adress</td><td>Contact #</td>
+	$( "#restore" ).click(function() {
+		$( ".ninjaImgShow" ).fadeIn( "slow", function() {
+		// Animation complete
+		});
+	});
+})	
